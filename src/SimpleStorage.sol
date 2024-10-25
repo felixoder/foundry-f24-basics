1d0bc81d1b6d019e0498492420bc1ab7c143352f
felixoder <debayanghosh408@gmail.com>
2024-10-25 15:11:40 +0530

    Created, Tested and Deployed the smart contract using the foundry framework

---
+++ src/SimpleStorage.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract SimpleStorage {
+    uint256 myFavouriteNumber;
+
+    struct Person {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    function store(uint256 _myFavouriteNumber) public {
+        myFavouriteNumber = _myFavouriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return myFavouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        listOfPeople.push(Person(_favouriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+}
